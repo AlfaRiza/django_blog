631f75c99b2885d5a5126faddbbe849626e84e6a
AlfaRiza <malfariza45@gmail.com>
2021-07-01 17:32:15 +0700

    migration

--- django_blog/myblog/models.py
+++ django_blog/myblog/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField()
+
+    def __str__(self):
+        return self.user.username
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    thumbnail = models.ImageField(null=True, blank=True)
+    featured = models.BooleanField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog', kwargs={
+            'blog_id': self.id
+        })
--- django_blog/myblog/views.py
+++ django_blog/myblog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
+
 
 # Create your views here.
+
+def index(request):
+    queryset = Post.objects.all()
+    context = {
+        'queryset': queryset,
+    }
+    return render(request, 'index.html', context)
+
+
+def blog(request):
+    # blog = get_object_or_404(Post, id = blog_id)
+    return render(request, 'blog.html')
