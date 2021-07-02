6931fb4a32f5cb405ed002558fcd008b84b92920
AlfaRiza <malfariza45@gmail.com>
2021-07-02 18:40:25 +0700

    ADD paginator model

--- django_blog/myblog/views.py
+++ django_blog/myblog/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 # Create your views here.
 
+
 def index(request):
-    queryset = Post.objects.all()
+    queryset = Post.objects.all().order_by('date')
+    paginator = Paginator(queryset, 2)
+    page_number = request.GET.get('page')
+    try:
+        data = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        data = paginator.get_page(1)
+    except EmptyPage:
+        data = paginator.get_page(1)
     context = {
         'queryset': queryset,
+        'data': data,
     }
     return render(request, 'index.html', context)
 
